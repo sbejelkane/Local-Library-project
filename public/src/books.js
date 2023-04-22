f86738ec9caf042e7f62d480f3ba8097e1ea9125
MSKane <60955965+sbejelkane@users.noreply.github.com>
2023-04-22 05:43:15 -0400

    Update books.js
    fixed bugs

--- public/src/books.js
+++ public/src/books.js
@@ -10,55 +10,39 @@ function findBookById(books, id) {
   return books.find(({ id: bookID }) => id == bookID)
 }
 
-//helper function
-const notReturned = [];
-const returned = [];
-const booksPartitioned = [notReturned, returned];
-
-function pushToAnArray(item) {
-	if (item === false) {
-		notReturned.push(item);
-	} else {
-		returned.push(item);
-	};
-}
-
 function partitionBooksByBorrowedStatus(books) {
+  const notReturned = [];
+    const returned = [];
+    
     for (let i = 0; i < books.length; i++) {
         let book = books[i]
-        pushToAnArray(book);
-      }
-    return booksPartitioned; 
-  }
-
-function helper(accounts) {
-    accounts.forEach(account => {
-        borrowList.forEach(({ id, returned }) => {
-            if (id == account.id) borrowers.push({ ...account, returned })
-        })
-    })    
-}  
-
-function getBorrowersForBook(book, accounts) {
-    const { borrows: borrowList } = book
-    //   let borrowers = []
-    let borrowers = helper(accounts)
-    return borrowers.slice(0, 10)
+        if (book.borrows[0].returned === false) {
+            notReturned.push(book);
+        } else {
+            returned.push(book);
+        }
+    }
+    const booksPartitioned = [notReturned, returned];
+    return booksPartitioned;
 }
 
-
-/* function getBorrowersForBook(book, accounts) {
-  const { borrows: borrowList } = book
-  let borrowers = []
-
+function helper(accounts, borrowList, borrowers) {
   accounts.forEach(account => {
       borrowList.forEach(({ id, returned }) => {
           if (id == account.id) borrowers.push({ ...account, returned })
       })
-  })
-  return borrowers.slice(0, 10)
+  })  
+  return borrowers.slice(0, 10);  
+}  
+
+function getBorrowersForBook(book, accounts) {
+  const { borrows: borrowList } = book
+  let borrowers = []
+  let result = helper(accounts, borrowList, borrowers);
+//  console.log("result", result);
+  return result
 }
-*/
+
 
 module.exports = {
   findAuthorById,
